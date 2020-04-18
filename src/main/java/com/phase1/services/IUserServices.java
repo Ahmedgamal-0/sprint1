00080a9c623b9910c2509472274f726f3d22f2ea
Ahmed Gamal <60451643+Ahmedgamal-0@users.noreply.github.com>
2020-04-18 20:54:12 +0200

    addlogin
    add login

--- src/main/java/com/phase1/services/IUserServices.java
+++ src/main/java/com/phase1/services/IUserServices.java
@@ -1,6 +1,8 @@
 package com.phase1.services;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.phase1.models.Admin;
 import com.phase1.models.Buyers;
 import com.phase1.models.StoreOwners;
@@ -11,6 +13,8 @@ public interface IUserServices {
 	public boolean addUser(Buyers newUser);
 	public boolean addStoreOwner(StoreOwners newUser);
 	public boolean addAdmin(Admin newUser);
-	
-	public List<Buyers> getAllUsers();
+	public boolean IsAdmin(String name,String pass);
+	public boolean IsBuyer(String name,String pass);
+	public boolean IsStoreOwner(String name,String pass);
+	public List<Buyers> getAllUsers(@RequestParam String name,@RequestParam String pass);
 }
--- src/main/java/com/phase1/services/UserServices.java
+++ src/main/java/com/phase1/services/UserServices.java
@@ -1,6 +1,8 @@
 package com.phase1.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -22,37 +24,73 @@ public class UserServices implements IUserServices{
 	private IAdminRepository admin;
 	@Autowired
 	private IStoreOwnerRepository storeowner;
-	
-	
+
+
 	@Override
 	public boolean addUser(Buyers newUser) {
-		
+
 		buyer.save(newUser);
 		return true;
 	}
 	@Override
 	public boolean addStoreOwner(StoreOwners newUser) {
-		
+
 		storeowner.save(newUser);
 		return true;
-		
+
 	}
 	@Override
 	public boolean addAdmin(Admin newUser) {
 		admin.save(newUser);
 		return true;
-		
+
 	}
 
+	  @Override
+		public boolean IsAdmin(String name,String pass) {
+		Iterable<Admin> usersIter= admin.findAll();
+		  for(Admin user: usersIter)
+		  {	if(user.getusername().equals(name) && user.getPassword().equals(pass))
+		  return true;
+		  }
+		  	return false;
+		  }
+	  @Override
+		  public boolean IsBuyer(String name,String pass)
+		  {
+		  Iterable<Buyers> usersIter= buyer.findAll();
+		  for(Buyers user: usersIter)
+		  {	if(user.getusername().equals(name) && user.getPassword().equals(pass))
+		  return true;
+		  }
+		  	return false;
+
+		  }
+	  @Override
+	     public boolean IsStoreOwner(String name,String pass) {
+
+		  Iterable<StoreOwners> usersIter= storeowner.findAll();
+		  for(StoreOwners user: usersIter) {
+		    if(user.getusername().equals(name) && user.getPassword().equals(pass))
+				  return true;
+				  }
+				  	return false;
+			}
+
+
+
 	@Override
-	public List<Buyers> getAllUsers() {
+	public List<Buyers> getAllUsers(@RequestParam String name,@RequestParam String pass) {
 		// TODO Auto-generated method stub
+		if(IsAdmin(name,pass)) {
 		Iterable<Buyers> usersIter=buyer.findAll();
 		List<Buyers> all= new ArrayList<Buyers>();
 		for(Buyers user: usersIter)
 			all.add(user);
-		
+
 		return all;
+		}
+		return null;
 	}
 
 }
